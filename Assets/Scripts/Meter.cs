30d73d02467ddef5a66a98952553d980141da440
eggison <12emiloo@gmail.com>
2022-02-20 20:31:30 -0500

    meter angle adjustment from +26 to +20

--- Assets/Scripts/Meter.cs
+++ Assets/Scripts/Meter.cs
@@ -37,7 +37,7 @@ public class Meter : MonoBehaviour
         // egchan: modified angle for masking/ bar fill to 26 deg
         // Max power is top and min power is bottom of meter, use lerp to place marker at current power
         float t = Mathf.InverseLerp(minThrowPower, maxThrowPower, throwPower);
-        Quaternion newAngle = Quaternion.Lerp(Quaternion.Euler(0.0f, 0.0f, 26.0f), Quaternion.Euler(0.0f, 0.0f, -26.0f), t);
+        Quaternion newAngle = Quaternion.Lerp(Quaternion.Euler(0.0f, 0.0f, 20.0f), Quaternion.Euler(0.0f, 0.0f, -26.0f), t);
 
         // Use local rotation, since marker is a child of the meter background.
         marker.transform.localRotation = newAngle;
